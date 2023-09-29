0f8106206951779fdcda503c2e49e5a64defcf43
Hackusate_PvP <Supermftw@gmail.com>
2023-09-29 04:24:11 -0500

    Revamped music tracks.

---
+++ src/main/java/me/piitex/renjava/api/music/Tracks.java
@@ -0,0 +1,45 @@
+package me.piitex.renjava.api.music;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Tracks {
+    private final Map<String, Track> tracks = new HashMap<>();
+
+    private Track currentTrack = null;
+
+    public void addTrack(String trackID, Track track) {
+        tracks.put(trackID, track);
+    }
+
+    public Track getTrack(String trackID) {
+        return tracks.get(trackID);
+    }
+
+    public Track getCurrentTrack() {
+        return currentTrack;
+    }
+
+    public void play(Track track) {
+        if (currentTrack != null) {
+            currentTrack.stop();
+
+        }
+        track.play();
+    }
+
+    public void stop() {
+        if (currentTrack != null) {
+            currentTrack.stop();
+        }
+    }
+
+    public void setCurrentTrack(Track currentTrack) {
+        this.currentTrack = currentTrack;
+    }
+
+    public Collection<Track> getTracks() {
+        return tracks.values();
+    }
+}
