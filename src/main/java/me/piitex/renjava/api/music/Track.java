705f74906ffafed8408d9111da66dea8889c9ecd
Hackusate_PvP <Supermftw@gmail.com>
2023-10-27 01:10:31 -0500

    Re-structured music api.

--- src/main/java/me/piitex/renjava/api/music/Track.java
+++ src/main/java/me/piitex/renjava/api/music/Track.java
@@ -1,5 +1,11 @@
 package me.piitex.renjava.api.music;
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+import me.piitex.renjava.RenJava;
+import me.piitex.renjava.api.APIChange;
+
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
@@ -7,27 +13,36 @@ import java.io.IOException;
 public class Track {
     private final File file;
 
-    private Clip clip;
+    private boolean loop;
+
+    private MediaPlayer player;
 
     public Track(File file) {
         this.file = file;
     }
 
-    public void play() {
-        try {
-            AudioInputStream inputStream = AudioSystem.getAudioInputStream(file);
-            Clip clip = AudioSystem.getClip();
-            clip.open(inputStream);
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-            this.clip = clip;
-        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-            e.printStackTrace();
+    @APIChange(description = "Switched to using MediaPlayer instead of Clip. Most audio files should be supported with this change.", changedVersion = "0.0.289")
+    protected void play(boolean loop) {
+        this.loop = loop;
+        player = new MediaPlayer(new Media(file.toURI().toString()));
+        if (loop) {
+            player.setOnEndOfMedia(() -> {
+                player.seek(Duration.ZERO);
+                player.play();
+            });
         }
+        RenJava.getInstance().getLogger().warning("Volume: " + RenJava.getInstance().getSettings().getVolume() / 500d);
+        player.setVolume(RenJava.getInstance().getSettings().getVolume() / 500d);
+        player.play();
+    }
+
+    public boolean isLoop() {
+        return loop;
     }
 
     public void stop() {
-        if (clip != null) {
-            clip.close();
+        if (player != null) {
+            player.stop();
         }
     }
 
