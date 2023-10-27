8fd2c570e9403222a58c65ae2738beca5e384a07
Hackusate_PvP <Supermftw@gmail.com>
2023-10-27 01:11:18 -0500

    Moved and deprecated StoryManager into Player.

--- src/main/java/me/piitex/renjava/api/stories/StoryManager.java
+++ src/main/java/me/piitex/renjava/api/stories/StoryManager.java
@@ -1,18 +1,29 @@
 package me.piitex.renjava.api.stories;
 
 import me.piitex.renjava.RenJava;
+import me.piitex.renjava.api.APIChange;
+import me.piitex.renjava.api.APINote;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Class dedicated to keeping track of the story. Useful for rolling back and loading.
+ *
+ * @deprecated Use {@link RenJava#getPlayer()} instead.
  */
+
+@Deprecated
+@APINote(description = "This class will be removed in the future.")
+@APIChange(description = "As of version 0.0.289 this class is marked as deprecated and will be removed for future versions.", changedVersion = "0.0.289")
 public class StoryManager {
-    private final Map<String, Story> storyIdMap = new HashMap<>();
 
     public Story getCurrentStory() {
         return RenJava.getInstance().getPlayer().getCurrentStory();
+    }
+
+    public void setCurrentStory(String id) {
+        setCurrentStory(getStory(id));
     }
 
     public void setCurrentStory(Story currentStory) {
@@ -20,10 +31,10 @@ public class StoryManager {
     }
 
     public void addStory(Story story) {
-        storyIdMap.put(story.getId(), story);
+        RenJava.getInstance().getPlayer().addStory(story);
     }
 
     public Story getStory(String id) {
-        return storyIdMap.get(id);
+        return RenJava.getInstance().getPlayer().getStory(id);
     }
 }
