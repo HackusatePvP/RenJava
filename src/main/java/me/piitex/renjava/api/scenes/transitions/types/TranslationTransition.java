6c9da5f10e9cce11b9e152155fed13da860b54a6
Hackusate_PvP <Supermftw@gmail.com>
2024-02-15 17:35:51 -0600

    Added Translation Transition.

---
+++ src/main/java/me/piitex/renjava/api/scenes/transitions/types/TranslationTransition.java
@@ -0,0 +1,76 @@
+package me.piitex.renjava.api.scenes.transitions.types;
+
+import javafx.animation.TranslateTransition;
+import javafx.scene.Node;
+import javafx.util.Duration;
+import me.piitex.renjava.api.scenes.transitions.Transitions;
+
+public class TranslationTransition extends Transitions {
+    private double byX, byY;
+    private double fromX, fromY;
+    private double toX, toY;
+
+    private TranslationTransition transition;
+
+    public TranslationTransition(double byX, double byY, double fromX, double fromY, double toX, double toY, int duration) {
+        super(duration);
+        this.byX = byX;
+        this.byY = byY;
+        this.fromX = fromX;
+        this.fromY = fromY;
+        this.toX = toX;
+        this.toY = toY;
+    }
+
+    public double getByX() {
+        return byX;
+    }
+
+    public double getByY() {
+        return byY;
+    }
+
+    public double getFromX() {
+        return fromX;
+    }
+
+    public double getFromY() {
+        return fromY;
+    }
+
+    public double getToX() {
+        return toX;
+    }
+
+    public double getToY() {
+        return toY;
+    }
+
+    public TranslationTransition getTransition() {
+        return transition;
+    }
+
+    @Override
+    public void play(Node node) {
+        TranslateTransition transition = new TranslateTransition();
+        transition.setByX(byX);
+        transition.setByY(byY);
+        transition.setFromX(fromX);
+        transition.setFromY(fromY);
+        transition.setToX(toX);
+        transition.setToY(toY);
+        transition.setNode(node);
+        transition.setDuration(Duration.seconds(getDuration()));
+        transition.setOnFinished(actionEvent -> {
+            if (getOnFinish() != null) {
+                getOnFinish().onEnd(actionEvent);
+            }
+        });
+        transition.play();
+    }
+
+    @Override
+    public void stop() {
+        transition.stop();
+    }
+}
