acba4e1ceb5b53dd6f75e415c84622e1f1075f79
Hackusate_PvP <Supermftw@gmail.com>
2024-05-04 13:48:38 -0500

    Added more rendering functions and converted ImageLoader to ImageOverlay.

--- src/main/java/me/piitex/renjava/gui/Menu.java
+++ src/main/java/me/piitex/renjava/gui/Menu.java
@@ -3,17 +3,14 @@ package me.piitex.renjava.gui;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import me.piitex.renjava.RenJava;
 
 import me.piitex.renjava.loggers.RenLogger;
-import me.piitex.renjava.api.builders.ImageLoader;
 import me.piitex.renjava.api.scenes.RenScene;
 import me.piitex.renjava.api.scenes.transitions.Transitions;
 import me.piitex.renjava.events.types.*;
-import me.piitex.renjava.gui.exceptions.ImageNotFoundException;
 import me.piitex.renjava.gui.layouts.Layout;
 import me.piitex.renjava.gui.overlay.*;
 import org.jetbrains.annotations.Nullable;
@@ -26,14 +23,17 @@ public class Menu {
     private final Stage stage;
     private Pane pane;
     private static Menu rootMenu;
-    private final double width, height;
-
-    private int x, y;
+    private double width, height;
+    private boolean renderFadeInFill = true;
+    private boolean renderFadeOutFill = true;
+    private double x, y;
+    private double scaleX;
+    private double scaleY;
 
     // Pre configured data.
     private String title = renJava.getName();
 
-    private Image backgroundImage;
+    private ImageOverlay backgroundImage;
 
     private final LinkedHashSet<Node> nodes = new LinkedHashSet<>();
     private final LinkedHashSet<Layout> layouts = new LinkedHashSet<>();
@@ -50,14 +50,8 @@ public class Menu {
         this.stage = renJava.getStage();
     }
 
-    public Menu(double width, double height, ImageLoader imageLoader) {
-        if (imageLoader != null) {
-            try {
-                this.backgroundImage = imageLoader.build();
-            } catch (ImageNotFoundException e) {
-                RenLogger.LOGGER.error(e.getMessage());
-            }
-        }
+    public Menu(double width, double height, ImageOverlay backgroundImage) {
+        this.backgroundImage = backgroundImage;
         this.width = width;
         this.height = height;
         this.stage = renJava.getStage();
@@ -75,15 +69,23 @@ public class Menu {
         return height;
     }
 
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
     public double getWidth() {
         return width;
     }
 
-    public int getX() {
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public double getX() {
         return x;
     }
 
-    public int getY() {
+    public double getY() {
         return y;
     }
 
@@ -95,15 +97,39 @@ public class Menu {
         this.y = y;
     }
 
+    public double getScaleX() {
+        return scaleX;
+    }
+
+    public void setScaleX(double scaleX) {
+        this.scaleX = scaleX;
+    }
+
+    public double getScaleY() {
+        return scaleY;
+    }
+
+    public void setScaleY(double scaleY) {
+        this.scaleY = scaleY;
+    }
+
+    public void setRenderFadeInFill(boolean renderFadeInFill) {
+        this.renderFadeInFill = renderFadeInFill;
+    }
+
+    public void setRenderFadeOutFill(boolean renderFadeOutFill) {
+        this.renderFadeOutFill = renderFadeOutFill;
+    }
+
     public String getTitle() {
         return title;
     }
 
-    public Image getBackgroundImage() {
+    public ImageOverlay getBackgroundImage() {
         return backgroundImage;
     }
 
-    public Menu setBackgroundImage(Image backgroundImage) {
+    public Menu setBackgroundImage(ImageOverlay backgroundImage) {
         this.backgroundImage = backgroundImage;
         return this;
     }
@@ -160,7 +186,19 @@ public class Menu {
     }
 
     public Pane render() {
-        return render(null, null);
+        return render(null, null, true);
+    }
+
+    public Pane render(boolean render) {
+        return render(null, null, render);
+    }
+
+    public Pane render(Pane pane) {
+        return render(pane, null, true);
+    }
+
+    public Pane render(RenScene renScene) {
+        return render(null, renScene, true);
     }
 
     /**
@@ -169,22 +207,38 @@ public class Menu {
      * @param root The root Group to which the menu will be added. If null, a new Group will be created.
      * @param renScene The RenScene that is being used. If null, it will be assumed this is a main menu screen.
      */
-    public Pane render(@Nullable Pane root, @Nullable RenScene renScene) {
+    public Pane render(@Nullable Pane root, @Nullable RenScene renScene, boolean render) {
         RenLogger.LOGGER.info("Rendering menu...");
         if (root == null) {
+            RenLogger.LOGGER.debug("Root pane is null.");
             root = new Pane();
         }
 
         root.setTranslateX(x);
         root.setTranslateY(y);
-        root.setPrefSize(width, height);
+        if (scaleX > 0) {
+            root.setPrefWidth(width * scaleX);
+            root.setMaxWidth(width * scaleX);
+            root.setMinWidth(width * scaleX);
+        }
+        if (scaleY > 0) {
+            root.setPrefHeight(height * scaleY);
+            root.setMaxHeight(height * scaleY);
+            root.setMinHeight(height * scaleY);
+        }
 
         // Background fill is used for fade ins.
-        BackgroundFill backgroundFill = new BackgroundFill(BLACK, new CornerRadii(1), new Insets(0,0,0,0));
-        root.setBackground(new Background(backgroundFill));
+        if (renderFadeInFill) {
+            BackgroundFill backgroundFill = new BackgroundFill(BLACK, new CornerRadii(1), new Insets(0, 0, 0, 0));
+            root.setBackground(new Background(backgroundFill));
+        }
 
         if (backgroundImage != null) {
-            Element backgroundImgElement = new Element(new ImageOverlay(backgroundImage, 0, 0));
+            ImageOverlay backGroundImageOverlay = backgroundImage;
+
+            backGroundImageOverlay.setScaleX(this.scaleX);
+            backGroundImageOverlay.setScaleY(this.scaleY);
+            Element backgroundImgElement = new Element(backGroundImageOverlay);
             backgroundImgElement.render(root);
         }
 
@@ -195,6 +249,9 @@ public class Menu {
 
         RenLogger.LOGGER.info("Rendering overlays...");
         for (Overlay overlay : overlays) {
+            overlay.setScaleX(scaleX);
+            overlay.setScaleY(scaleY);
+
             new Element(overlay).render(root);
         }
 
@@ -204,35 +261,47 @@ public class Menu {
         }
 
         for (Menu menu : children) {
+            RenLogger.LOGGER.info("Rendering child menus...");
             if (menu != null) {
-                menu.render(root, renScene); // Renders menu on top of this menu.
+                menu.setRenderFadeOutFill(renderFadeOutFill);
+                menu.setRenderFadeInFill(renderFadeInFill);
+                menu.setScaleX(scaleX);
+                menu.setScaleY(scaleY);
+                menu.render(root, renScene, render); // Renders menu on top of this menu.
             }
         }
 
         rootMenu = this;
 
-        Scene scene;
-        if (stage.getScene() != null) {
-            scene = stage.getScene();
-            stage.getScene().setRoot(root);
-        } else {
-            stage.setScene(new Scene(root));
-            scene = stage.getScene();
+        if (render) {
+            Scene scene;
+            if (stage.getScene() != null) {
+                scene = stage.getScene();
+                stage.getScene().setRoot(root);
+            } else {
+                stage.setScene(new Scene(root));
+                scene = stage.getScene();
+            }
+
+            setInputControls(scene);
+
+            this.pane = root;
+
+            if (renderFadeOutFill) {
+                scene.setFill(BLACK); // Scene fill is used for fade outs.
+            }
+
+            // Apply transition to root
+            if (renScene != null && renScene.getStartTransition() != null) {
+                Transitions transitions = renScene.getStartTransition();
+                transitions.play(root);
+            }
+            if (renScene != null) {
+                renScene.setStage(stage);
+                RenJava.getInstance().getPlayer().setLastRenderedRenScene(renScene);
+            }
+            stage.show();
         }
-
-        setInputControls(scene);
-
-        this.pane = root;
-
-        scene.setFill(BLACK); // Scene fill is used for fade outs.
-
-        // Apply transition to root
-        if (renScene != null && renScene.getStartTransition() != null) {
-            Transitions transitions = renScene.getStartTransition();
-            transitions.play(root);
-        }
-
-        stage.show();
 
         return root;
     }
