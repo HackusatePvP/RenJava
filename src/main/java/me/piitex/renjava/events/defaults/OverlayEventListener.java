0592727e4fb9fb587a552c40d3026e7a9d29dade
Hackusate_PvP <Supermftw@gmail.com>
2024-05-24 12:19:45 -0500

    Added events SliderChangeEvent and OverlayClickReleaseEvent.

--- src/main/java/me/piitex/renjava/events/defaults/OverlayEventListener.java
+++ src/main/java/me/piitex/renjava/events/defaults/OverlayEventListener.java
@@ -2,11 +2,10 @@ package me.piitex.renjava.events.defaults;
 
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import me.piitex.renjava.RenJava;
 import me.piitex.renjava.events.EventListener;
 import me.piitex.renjava.events.Listener;
-import me.piitex.renjava.events.types.OverlayClickEvent;
-import me.piitex.renjava.events.types.OverlayExitEvent;
-import me.piitex.renjava.events.types.OverlayHoverEvent;
+import me.piitex.renjava.events.types.*;
 import me.piitex.renjava.gui.exceptions.ImageNotFoundException;
 import me.piitex.renjava.gui.overlay.ButtonOverlay;
 import me.piitex.renjava.gui.overlay.Overlay;
@@ -37,6 +36,9 @@ public class OverlayEventListener implements EventListener {
             if (buttonOverlay.isHover()) {
                 if (buttonOverlay.getHoverColor() != null) {
                     button.setTextFill(buttonOverlay.getHoverColor());
+                } else {
+                    System.out.println("Setting default hover color for: " + buttonOverlay.getId());
+                    button.setTextFill(RenJava.getInstance().getConfiguration().getHoverColor());
                 }
                 if (buttonOverlay.getHoverImage() != null) {
                     try {
@@ -45,7 +47,17 @@ public class OverlayEventListener implements EventListener {
                         RenLogger.LOGGER.error(e.getMessage());
                     }
                 }
+            } else {
+                System.out.println("No hover was set for overlay");
             }
+        }
+    }
+
+    @Listener
+    public void onOverlayRelease(OverlayClickReleaseEvent event) {
+        Overlay overlay = event.getOverlay();
+        if (overlay.getOnRelease() != null) {
+            overlay.getOnRelease().onClickRelease(event);
         }
     }
 
