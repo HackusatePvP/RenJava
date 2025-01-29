5ec363ed3dc4741b3a2d1220c619136e54a1abbb
HackusatePvP <no@email.com>
2025-01-29 07:36:28 -0600

    Added resource extraction utility.

---
+++ src/main/java/me/piitex/renjava/utils/ResourceUtil.java
@@ -0,0 +1,188 @@
+package me.piitex.renjava.utils;
+
+import me.piitex.renjava.RenJava;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+
+public class ResourceUtil {
+
+    public static int extractFromResources(String filter, File outputDir) {
+        return extractFromResources(filter, null, outputDir, null);
+    }
+
+    public static int extractFromResources(String filter, List<String> remove, File outputDir) {
+        return extractFromResources(filter, remove, outputDir, null);
+    }
+
+    public static int extractFromResources(String filter, List<String> remove, File outputDir, File copyDir) {
+        int extracted = 0;
+        Collection<String> files = ResourceUtil.getResources(Pattern.compile(".*"));
+        for (String s : files) {
+            if (s.startsWith(filter)) {
+                InputStream inputStream = RenJava.class.getClassLoader().getResourceAsStream(s);
+                File file;
+                if (remove != null && !remove.isEmpty()) {
+                    for (String r : remove) {
+                        s = s.replace(r, "");
+                    }
+                }
+                if (s.endsWith("/")) {
+                    file = new File(outputDir, s + "/");
+                    file.mkdir();
+                } else {
+                    file = new File(outputDir, s);
+                    try {
+                        IOUtils.copy(inputStream, new FileOutputStream(file));
+                        if (copyDir != null) {
+                            FileUtils.copyFile(file, new File(copyDir, s));
+                        }
+                        extracted++;
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+
+        return extracted;
+    }
+
+    public static void copyDirectory(File sourceDirectory, File destinationDirectory) throws IOException {
+        if (!destinationDirectory.exists()) {
+            destinationDirectory.mkdir();
+        }
+        for (String f : sourceDirectory.list()) {
+            copyDirectoryCompatibityMode(new File(sourceDirectory, f), new File(destinationDirectory, f));
+        }
+    }
+
+    public static void copyDirectoryCompatibityMode(File source, File destination) throws IOException {
+        if (source.isDirectory()) {
+            copyDirectory(source, destination);
+        } else {
+            copyFile(source, destination);
+        }
+    }
+
+    private static void copyFile(File sourceFile, File destinationFile)
+            throws IOException {
+        try (InputStream in = new FileInputStream(sourceFile);
+             OutputStream out = new FileOutputStream(destinationFile)) {
+            byte[] buf = new byte[1024];
+            int length;
+            while ((length = in.read(buf)) > 0) {
+                out.write(buf, 0, length);
+            }
+        }
+    }
+
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
+    }
+
+    public static boolean deleteDirectory(File directoryToBeDeleted) {
+        File[] allContents = directoryToBeDeleted.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteDirectory(file);
+            }
+        }
+        return directoryToBeDeleted.delete();
+    }
+
+    /**
+     * for all elements of java.class.path get a Collection of resources Pattern
+     * pattern = Pattern.compile(".*"); gets all resources
+     *
+     * @param pattern the pattern to match
+     * @return the resources in the order they are found
+     */
+    public static Collection<String> getResources(final Pattern pattern) {
+        final ArrayList<String> retval = new ArrayList<>();
+        final String classPath = System.getProperty("java.class.path", ".");
+        final String[] classPathElements = classPath.split(System.getProperty("path.separator"));
+        for (final String element : classPathElements) {
+            retval.addAll(getResources(element, pattern));
+        }
+        return retval;
+    }
+
+    private static Collection<String> getResources(final String element, final Pattern pattern) {
+        final ArrayList<String> retval = new ArrayList<>();
+        final File file = new File(element);
+        if (file.isDirectory()) {
+            retval.addAll(getResourcesFromDirectory(file, pattern));
+        } else {
+            retval.addAll(getResourcesFromJarFile(file, pattern));
+        }
+        return retval;
+    }
+
+    private static Collection<String> getResourcesFromJarFile(final File file, final Pattern pattern) {
+        final ArrayList<String> retval = new ArrayList<String>();
+        ZipFile zf;
+        try {
+            zf = new ZipFile(file);
+        } catch (final ZipException e) {
+            throw new Error(e);
+        } catch (final IOException e) {
+            throw new Error(e);
+        }
+        final Enumeration e = zf.entries();
+        while (e.hasMoreElements()) {
+            final ZipEntry ze = (ZipEntry) e.nextElement();
+            final String fileName = ze.getName();
+            final boolean accept = pattern.matcher(fileName).matches();
+            if (accept) {
+                retval.add(fileName);
+            }
+        }
+        try {
+            zf.close();
+        } catch (final IOException e1) {
+            throw new Error(e1);
+        }
+        return retval;
+    }
+
+    private static Collection<String> getResourcesFromDirectory(final File directory, final Pattern pattern) {
+        final ArrayList<String> retval = new ArrayList<String>();
+        final File[] fileList = directory.listFiles();
+        for (final File file : fileList) {
+            if (file.isDirectory()) {
+                retval.addAll(getResourcesFromDirectory(file, pattern));
+                file.mkdir();
+            } else {
+                try {
+                    final String fileName = file.getCanonicalPath();
+                    final boolean accept = pattern.matcher(fileName).matches();
+                    if (accept) {
+                        retval.add(fileName);
+                    }
+                } catch (final IOException e) {
+                    throw new Error(e);
+                }
+            }
+        }
+        return retval;
+    }
+}
