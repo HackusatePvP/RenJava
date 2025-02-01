4328533058136d40c683cc07555e54fc5580985a
HackusatePvP <no@email.com>
2025-01-31 21:52:50 -0600

    Added comments.

--- src/main/java/me/piitex/renjava/Main.java
+++ src/main/java/me/piitex/renjava/Main.java
@@ -2,6 +2,11 @@ package me.piitex.renjava;
 
 public class Main {
 
+    /**
+     * Initializes the javafx application.
+     *
+     * @param args Application parameters.
+     */
     public static void main(String[] args) {
         Launch.main(args);
     }
