f91ea9602b01b393d6302e9dbd5fa49b81666461
HackusatePvP <no@email.com>
2025-09-12 18:21:32 -0500

    Added fading transitions will now stop at the end of the video.

--- src/main/java/me/piitex/renjava/api/scenes/transitions/types/FadingTransition.java
+++ src/main/java/me/piitex/renjava/api/scenes/transitions/types/FadingTransition.java
@@ -1,7 +1,11 @@
 package me.piitex.renjava.api.scenes.transitions.types;
 
 import javafx.animation.FadeTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.scene.Node;
+import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
 import me.piitex.renjava.RenJava;
@@ -82,6 +86,10 @@ public class FadingTransition extends Transitions {
         });
         playing = true;
         RenJava.PLAYER.setCurrentTransition(this);
+        if (root instanceof MediaView mediaView) {
+            Timeline timeline = new Timeline(new KeyFrame(Duration.valueOf(getDuration() + "ms"), new KeyValue(mediaView.getMediaPlayer().volumeProperty(), toValue)));
+            timeline.play();
+        }
         fadeTransition.play();
     }
 
